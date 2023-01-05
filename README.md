5c6754b29e89bfe79bf983da9881de82964063cf
klesniak1 <klesniak@tascent.com>
2023-01-05 13:24:38 +0100

    Added SAR example usage with springboot3.

--- README.md
+++ README.md
@@ -1,1 +1,25 @@
-# specification-arg-resolver-example-springboot3
+some examples for SpecificationArgumentResolver with Spring Boot 3
+===============================================
+
+This is a simple Spring Boot project which presents [Specification Argument Resolver library](https://github.com/tkaczmarzyk/specification-arg-resolver).
+
+It is an executable jar with embedded H2 db, so just build it with Maven and explore the API. You will find some samples below:
+
+1. Get all customers:
+
+   curl http://localhost:8080/customers
+
+2. Delete a customer
+
+   curl -X DELETE http://localhost:8080/customers/3
+
+   it will perform a soft delete, which you can verify by getting all customers again -- the deleted customer will be still there, but with `deleted = true` flag. As you will see in subsequent points, specification-based query methods will filter it out.
+
+3. Filter customers (include only not deleted ones) by first name and gender (optionally):
+
+   curl http://localhost:8080/customers?firstName=ar
+   curl 'http://localhost:8080/customers?firstName=ar&gender=MALE'
+
+4. Filter by name (either first or last, case insensitive) with paging:
+
+   curl 'http://localhost:8080/customers?name=l&page=0&size=2&sort=id'
---
+++ src/main/java/net/kaczmarzyk/example/DataInit.java
@@ -0,0 +1,52 @@
+package net.kaczmarzyk.example;
+
+import java.util.Calendar;
+import java.util.Date;
+
+import jakarta.annotation.PostConstruct;
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.PersistenceContext;
+
+import net.kaczmarzyk.example.domain.Customer;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+import org.springframework.transaction.PlatformTransactionManager;
+import org.springframework.transaction.TransactionStatus;
+import org.springframework.transaction.support.TransactionCallbackWithoutResult;
+import org.springframework.transaction.support.TransactionTemplate;
+
+
+@Component
+public class DataInit {
+
+    @PersistenceContext
+    private EntityManager em;
+    
+    @Autowired
+    private PlatformTransactionManager txManager;
+    
+    
+    @PostConstruct
+    public void initializeData() {
+        new TransactionTemplate(txManager).execute(new TransactionCallbackWithoutResult() {
+            @Override
+            protected void doInTransactionWithoutResult(TransactionStatus arg0) {
+                em.persist(new Customer("Homer", "Simpson", "M", date(2014, Calendar.JANUARY, 01)));
+                em.persist(new Customer("Marge", "Simpson", "F", date(2014, Calendar.JANUARY, 07)));
+                em.persist(new Customer("Bart", "Simpson", "M", date(2014, Calendar.JANUARY, 14)));
+                em.persist(new Customer("Lisa", "Simpson", "F", date(2014, Calendar.JANUARY, 21)));
+                em.persist(new Customer("Barney", "Gumble", "M", date(2014, Calendar.JANUARY, 22)));
+                em.persist(new Customer("Moe", "Szyslak", "M", date(2014, Calendar.JANUARY, 23)));
+            }
+        });
+    }
+
+    private Date date(int year, int month, int day) {
+        Calendar cal = Calendar.getInstance();
+        cal.set(Calendar.YEAR, year);
+        cal.set(Calendar.MONTH, month);
+        cal.set(Calendar.DAY_OF_MONTH, day);
+        return cal.getTime();
+    }
+}
